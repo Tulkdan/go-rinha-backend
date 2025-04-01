9ac29164feb1be2d4ed21d91020b0cdf9e02c86f
Pedro Correa <pedro8correa@gmail.com>
2025-03-31 23:02:10 -0300

    flake files

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A Nix-flake-based Go development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (pkgs.lib) optional optionals;
+
+        pkgs = import nixpkgs { inherit system; };
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go
+              golangci-lint
+              sqlc
+            ];
+          };
+        }
+    );
+}
